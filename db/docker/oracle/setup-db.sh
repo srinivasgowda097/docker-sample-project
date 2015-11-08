e9d42a48f9b1bac886f73bdf552f268c0880c692
Stephen Lou Banal <stephen.banal@gmail.com>
2015-11-08 23:33:04 +1100

    Add support for oracle

---
+++ db/docker/oracle/setup-db.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+/u01/app/oracle/product/11.2.0/xe/bin/sqlplus root/admin@localhost @/tmp/testdb.sql
+
---
+++ db/docker/oracle/testdb.sql
@@ -0,0 +1,2 @@
+CREATE TABLE hits (count number(10) not null);
+INSERT INTO hits (count) VALUES(0);
