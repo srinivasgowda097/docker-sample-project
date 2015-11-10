838b79cce1ba68fd99a33d0bdfea89db87054684
Stephen Lou Banal <stephen.banal@gmail.com>
2015-11-10 22:42:43 +1100

    Add support for oracle initialization script

--- db/docker/oracle/init.sql
+++ db/docker/oracle/init.sql
@@ -1,2 +1,7 @@
+
+CREATE USER dev IDENTIFIED BY d3v123;
 CREATE TABLE hits (count number(10) not null);
 INSERT INTO hits (count) VALUES(0);
+COMMIT;
+EXIT;
+
--- db/docker/oracle/setup-db.sh
+++ db/docker/oracle/setup-db.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-/u01/app/oracle/product/11.2.0/xe/bin/sqlplus root/admin@localhost @/tmp/testdb.sql
+export ORACLE_HOME=/u01/app/oracle/product/11.2.0/xe
+export PATH=$ORACLE_HOME/bin:$PATH
+export ORACLE_SID=XE
+
+/u01/app/oracle/product/11.2.0/xe/bin/sqlplus system/oracle@localhost @/tmp/init.sql
 
