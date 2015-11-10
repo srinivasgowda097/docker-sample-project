5a2449d3e0a87e057046fb15378e96556f53b904
Stephen Lou Banal <stephen.banal@gmail.com>
2015-11-10 23:07:03 +1100

    Add oracle support

---
+++ web/docker/oracle/app.py
@@ -0,0 +1,29 @@
+from flask import Flask
+from redis import Redis
+import cx_Oracle
+
+app = Flask(__name__)
+app.debug = True
+redis = Redis(host='redis', port=6379)
+
+@app.route('/hello')
+def hello():
+    redis.incr('hits')
+    return 'Hello World! I have been seen %s times.' % redis.get('hits')
+
+@app.route('/hellodb')
+def helloDb():
+	db = cx_Oracle.connect('system/oracle@db:1521/XE')
+	data = db.version
+	cursor = db.cursor()
+	cursor.execute("UPDATE hits set count = count + 1")
+	db.commit()
+	cursor.execute("SELECT count FROM hits")
+	row = cursor.fetchone()
+	return 'Hello World Oracl DB! I have been seen {} times. Database version : {} '.format(row[0], data);
+	cursor.close
+	db.close()
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
+	
